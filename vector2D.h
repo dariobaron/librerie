c495f0dfe900e63f85ca4ca4169df739ee6b6348
dariobaron <barone.dario97@gmail.com>
2020-04-25 18:56:26 +0200

    correzioni vector2D

--- vector2D.h
+++ vector2D.h
@@ -47,16 +47,16 @@ public:
 	}
 	void insertRow(int r, const vector<T> & input){
 		if( checkColSize(input.size()) ){
-			v.insert(v.begin() + r*cols, input.begin(), input.end());
 			++rows;
+			v.insert(v.begin() + r*cols, input.begin(), input.end());
 		}
 	}
 	void insertCol(int c, const vector<T> & input){
 		if( checkRowSize(input.size()) ){
-			for(int i = 0; i < input.size(); ++i){
-				v.insert(v.begin() + i*cols + c, input.begin() + i);
-			}
 			++cols;
+			for(int i = 0; i < input.size(); ++i){
+				v.insert(v.begin() + i*cols + c, *input.begin() + i);
+			}
 		}
 	}
 	void eraseRow(int r){
@@ -71,16 +71,16 @@ public:
 	}
 	void pushBackRow(const vector<T> & input){
 		if( checkColSize(input.size()) ){
-			v.insert(v.end(), input.begin(), input.end());
 			++rows;
+			v.insert(v.end(), input.begin(), input.end());
 		}
 	}
 	void pushBackCol(const vector<T> & input){
 		if( checkRowSize(input.size()) ){
-			for(int i = 0; i < input.size(); ++i){
-				v.insert(v.begin() + i*cols - 1, input[i]);
-			}
 			++cols;
+			for(int i = 0; i < input.size(); ++i){
+				v.insert(v.begin() + (i+1)*cols - 1, input[i]);
+			}
 		}
 	}
 	void print(ostream & os) const{
